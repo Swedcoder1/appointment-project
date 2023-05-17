a95e946715517bb26387ec2eec4813f41463cf9b
Swedcoder <swedcoder@gmail.com>
2023-05-17 22:11:55 +0200

    Added tooltip to the icons

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -95,6 +95,9 @@ export default function Navbar() {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                    <rect className="w-full h-full opacity-0">
+                      <title>Appointments</title>
+                    </rect>
                   </svg>
                   <span class="flex-1 ml-3 whitespace-nowrap">
                     Appointments
@@ -119,6 +122,9 @@ export default function Navbar() {
                       d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                       clip-rule="evenodd"
                     ></path>
+                    <rect className="w-full h-full opacity-0">
+                      <title>Patients</title>
+                    </rect>
                   </svg>
                   <span class="flex-1 ml-3 whitespace-nowrap">Patients</span>
                 </Link>
@@ -137,6 +143,9 @@ export default function Navbar() {
                     d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
                     clip-rule="evenodd"
                   ></path>
+                  <rect className="w-full h-full opacity-0">
+                    <title>Sign out</title>
+                  </rect>
                 </svg>
                 <button
                   onClick={() => signOut()}
