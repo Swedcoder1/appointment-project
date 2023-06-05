89367bda0d11567207f4752e95e1350ebfed41bd
swedcoder <swedcoder@gmail.com>
2023-06-05 23:18:46 +0200

    Changed to capital letter

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
-import LoginBtn from "./components/login-btn";
+import LoginBtn from "./components/Login-btn";
 
 export default function Home() {
   return (
