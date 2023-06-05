12bc64f64efae2858f4037caa095cbe5fb3b351e
swedcoder <swedcoder@gmail.com>
2023-06-05 23:51:09 +0200

    added classname

--- app/components/patientComponents/SearchPatient.js
+++ app/components/patientComponents/SearchPatient.js
@@ -6,24 +6,24 @@ const SearchPatient = () => {
       <form>
         <label
           for="default-search"
-          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Search
         </label>
-        <div class="relative">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <HiOutlineSearch className="text-2xl" />
           </div>
           <input
             type="search"
             id="default-search"
-            class="block w-full p-4 pl-10 text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-full p-4 pl-10 text-gray-900 border-b-2 border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
             placeholder="Search patients.."
             required
           />
           <button
             type="submit"
-            class="text-white absolute right-2.5 bottom-2.5 bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+            className="text-white absolute right-2.5 bottom-2.5 bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
           >
             Search
           </button>
