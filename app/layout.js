5091f730f6aeccad3339b64e58a334f8d0640ba8
swedcoder <swedcoder@gmail.com>
2023-06-26 21:35:44 +0200

    Changed to AuthProviders as name component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Providers from "./providers";
+import AuthProviders from "./authproviders";
 import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,10 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+        <AuthProviders>
           <Navbar />
           <div className="md:!pl-16">{children}</div>
-        </Providers>
+        </AuthProviders>
       </body>
     </html>
   );
