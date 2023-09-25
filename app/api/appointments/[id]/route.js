b379edb82329aef8005d0f198b9d8efa80dc2509
swedcoder <swedcoder@gmail.com>
2023-09-25 21:26:09 +0200

    Added delete method for appointments

---
+++ app/api/appointments/[id]/route.js
@@ -0,0 +1,19 @@
+import dbConnect from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import Appointments from "@/app/schema/mongoose/appointmentModel";
+
+export async function DELETE(req) {
+  dbConnect();
+  console.log(req);
+
+  const id = await req.url.split("appointments/")[1];
+  console.log("id " + id);
+  const deleteAppointment = await Appointments.findByIdAndDelete(id);
+  console.log(deleteAppointment);
+
+  if (deleteAppointment) {
+    return NextResponse.json({ status: 200, message: "Deleted successfully" });
+  } else {
+    return NextResponse.json({ status: 500, message: "Couldn´t delete" });
+  }
+}
