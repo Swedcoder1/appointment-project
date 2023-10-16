b13235fc1538518a41c4f0aac36b676dcfc65df8
swedcoder <swedcoder@gmail.com>
2023-10-16 21:34:51 +0200

    Delete function fot medical records

--- app/components/journal/patientJournals.js
+++ app/components/journal/patientJournals.js
@@ -1,25 +1,83 @@
+"use client";
+import { useState } from "react";
+import UpdateJournal from "./updateJournal";
+
 export default function PatientJournal({ journalData }) {
+  const [openUpdateJournal, setOpenUpdateJournal] = useState(false);
+  const [updateJournal, setUpdateJournal] = useState(null);
+
   console.log("From PatientJournal " + JSON.stringify(journalData, null, " "));
+
+  const deleteJournal = (journalId) => {
+    alert(journalId);
+    fetch("http://localhost:3000/api/journal/" + journalId, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        res.json();
+        // router.refresh();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const findId = (journal) => {
+    console.log(journal);
+    setUpdateJournal(journal);
+    setOpenUpdateJournal(true);
+  };
+
   return (
-    <div>
-      {journalData.map((journal) => (
-        <div
-          className="mb-6 w-11/12 m-auto outline-1 outline rounded-sm"
-          key={journal._id}
-        >
-          <div className="bg-gray-100 pl-4 py-2">
-            <p className="font-semibold">Therapiest: {journal.therapiest}</p>
-            {/* <p>{journal.createdAt}</p> */}
-          </div>
-          <div className="grid space-y-2 ml-4">
-            <p>Medicalhistory: "{journal.journals.medicalHistory}"</p>
-            <p>Assessment: "{journal.journals.assessment}"</p>
-            <p>Medicaldiagnosis: "{journal.journals.medicalDiagnosis}"</p>
-            <p>Exercise: "{journal.journals.exercise}"</p>
-            <p>Follow-up:"{journal.journals.followUp}"</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        {journalData.map((journal) => {
+          //Get date when the journal was saved
+          const showDate = JSON.stringify(journal.createdAt).slice(1, 11);
+          const journalId = journal._id.toString();
+          return (
+            <>
+              <div
+                className="mb-6 w-11/12 m-auto outline-1 outline rounded-sm"
+                key={journalId}
+              >
+                <div className="bg-gray-100 pl-4 py-2">
+                  <div>
+                    <p className="font-semibold">
+                      Therapiest: {journal.therapiest}
+                    </p>
+                    <p>{showDate}</p>
+                  </div>
+                  <div>
+                    {journal.signed ? (
+                      <p className="text-green-500">Signed</p>
+                    ) : (
+                      <div>
+                        <p className="text-red-500">Not signed</p>
+                        <button onClick={() => deleteJournal(journalId)}>
+                          Delete
+                        </button>
+                        <button onClick={() => findId(journal)}>update</button>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="grid space-y-2 ml-4">
+                  <p>Medicalhistory: "{journal.journals.medicalHistory}"</p>
+                  <p>Assessment: "{journal.journals.assessment}"</p>
+                  <p>Medicaldiagnosis: "{journal.journals.medicalDiagnosis}"</p>
+                  <p>Exercise: "{journal.journals.exercise}"</p>
+                  <p>Follow-up:"{journal.journals.followUp}"</p>
+                </div>
+              </div>
+              {openUpdateJournal && (
+                <UpdateJournal updateJournal={updateJournal} />
+              )}
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
