3f751dd2d8f6875bf96315e967369b1b65456231
swedcoder <swedcoder@gmail.com>
2023-11-12 18:01:04 +0100

    changed code in POST request

--- app/api/patient/route.js
+++ app/api/patient/route.js
@@ -5,6 +5,9 @@ import Patient from "@/app/schema/mongoose/patientModel";
 export async function GET() {
   try {
     await dbConnect();
+    // const session = await getServerSession();
+    // const userName = session;
+    // console.log("Username:" + userName);
     const allPatients = await Patient.find({});
     console.log(allPatients);
     return NextResponse.json(allPatients);
@@ -18,26 +21,30 @@ export async function POST(req) {
     await dbConnect();
     const body = await req.json();
     const patientData = body;
+    console.log("Patientdata " + JSON.stringify(patientData));
     const checkPatient = await Patient.findOne({
+      firstName: patientData.firstName,
+      lastName: patientData.lastName,
       dateOfBirth: patientData.dateOfBirth,
+      therapiest: patientData.therapiest,
     });
 
     if (!checkPatient) {
       console.log("Does not exist");
       await Patient.create(patientData);
-      return NextResponse.json({ status: 200, message: "Patient created" });
+      return NextResponse.json({ message: "Patient created" }, { status: 200 });
     } else if (checkPatient) {
       console.log("Does exist");
-      return NextResponse.json({
-        status: 409,
-        message: "Patient already exist",
-      });
+      return NextResponse.json(
+        { message: "Patient already exist" },
+        { status: 409 }
+      );
     }
   } catch (error) {
     console.log(error);
-    return NextResponse.json({
-      status: 500,
-      message: "Patient not created, try again",
-    });
+    return NextResponse.json(
+      { message: "Patient not created, try again" },
+      { status: 500 }
+    );
   }
 }
