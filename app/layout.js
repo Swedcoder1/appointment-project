2b7fb0efe532b7b7e4b6d5373f950ed50637e42e
swedcoder <swedcoder@gmail.com>
2023-11-12 17:57:04 +0100

    Added toastercontainer

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import AuthProviders from "./authproviders";
 import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,14 @@ export default function RootLayout({ children }) {
         <AuthProviders>
           <Navbar />
           <div className="md:!pl-16">{children}</div>
+          <ToastContainer
+            position="top-center"
+            autoClose={1000}
+            pauseOnHover={false}
+            draggable={false}
+            hideProgressBar={true}
+            closeOnClick={true}
+          />
         </AuthProviders>
       </body>
     </html>
