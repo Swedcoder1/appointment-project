fb20b4b69158ce0ac68db5e3b951d36df91da209
swedcoder <swedcoder@gmail.com>
2023-11-12 17:59:52 +0100

    Added update function for the journals

--- app/api/journal/[id]/route.js
+++ app/api/journal/[id]/route.js
@@ -19,3 +19,43 @@ export async function DELETE(req) {
     });
   }
 }
+
+export async function POST(req) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+    // console.log("no stringify" + body);
+    console.log("Request: " + JSON.stringify(body));
+    const journalData = body;
+    const journalId = journalData.journalId;
+
+    const updatedJournal = await Journals.findOneAndUpdate(
+      { _id: journalId },
+      {
+        therapiest: journalData.therapiest,
+        journals: {
+          medicalHistory: journalData.medicalHistory,
+          assessment: journalData.assessment,
+          medicalDiagnosis: journalData.medicalDiagnosis,
+          exercise: journalData.exercise,
+          followUp: journalData.followUp,
+        },
+        signed: journalData.signed,
+      },
+      { new: true } // Set the 'new' option to return the updated document
+    );
+
+    // console.log("Updated " + updatedJournal);
+    if (!updatedJournal) {
+      return NextResponse.json({
+        status: 404,
+        message: "Medicalrecord not found",
+      });
+    }
+
+    return NextResponse.json({ status: 200, message: "Medicalrecord updated" });
+  } catch (error) {
+    console.log(error);
+  }
+}
