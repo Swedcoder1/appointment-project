db3f51c4e1aed0067fd105298752159a241ffc8f
swedcoder <swedcoder@gmail.com>
2023-11-12 18:24:58 +0100

    Added id to Fields

--- app/dashboard/patients/[patientId]/newJournal/page.js
+++ app/dashboard/patients/[patientId]/newJournal/page.js
@@ -5,10 +5,11 @@ import { historySchema } from "@/app/schema/yup/patientSchema";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/dist/server/api-utils";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function page({ params }) {
   const { data: session } = useSession();
+  const [checkSigned, setCheckSigned] = useState(false);
   const therapiestName = session?.user?.name;
 
   const patientId = params.patientId;
@@ -26,10 +27,11 @@ export default function page({ params }) {
             followUp: "",
             patientId: patientId,
             therapiest: therapiestName,
+            signed: checkSigned,
           }}
           validationSchema={historySchema}
           onSubmit={(values, { resetForm }) => {
-            // alert(JSON.stringify(values));
+            values.signed = checkSigned;
             fetch("http://localhost:3000/api/journal", {
               method: "POST",
               headers: {
@@ -37,79 +39,87 @@ export default function page({ params }) {
               },
               body: JSON.stringify(values),
               cache: "no-store",
-            });
-
-            router.push(`/dashboard/patients/${patientId}`);
-            router.refresh();
+            })
+              .then((res) => {
+                res.json();
+                router.push(`/dashboard/patients/${patientId}`);
+                router.refresh();
+              })
+              .catch((error) => {
+                console.log(error);
+              });
           }}
         >
-          {({ isSubmitting }) => (
+          {({ isSubmitting, values }) => (
             <div className="grid w-screen">
-              <div className="flex w-7/12 space-x-4 mt-6 text-lg ml-4">
-                <button
-                  className="bg-green-500 text-white py-2 px-12 rounded-md"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Save & Sign
-                </button>
-                <button className="outline outline-green-500 py-2 px-8 rounded-md">
-                  Save & don´t sign
-                </button>
-              </div>
               <Form className="w-7/12 ml-4 mt-4 space-y-4">
                 <div className="grid">
-                  <label htmlFor="medical-history">Medical history</label>
+                  <label htmlFor="medicalHistory">Medical history</label>
                   <Field
                     name="medicalHistory"
+                    id="medicalHistory"
                     as="textarea"
-                    className="border-gray-500 border-2 h-28 rounded-md"
+                    className="border-gray-500 border-2 h-28 rounded-md pl-1"
                   />
                 </div>
                 <div className="grid">
                   <label htmlFor="assessment">Assessment</label>
                   <Field
                     name="assessment"
+                    id="assessment"
                     as="textarea"
-                    className="border-gray-500 border-2 h-28 rounded-md"
+                    className="border-gray-500 border-2 h-28 rounded-md pl-1"
                   />
                 </div>
                 <div className="grid">
-                  <label htmlFor="medical-diagnosis">Medical diagnosis</label>
+                  <label htmlFor="medicalDiagnosis">Medical diagnosis</label>
                   <Field
                     name="medicalDiagnosis"
+                    id="medicalDiagnosis"
                     type="text"
-                    className="border-gray-500 border-2 h-12 rounded-md"
+                    className="border-gray-500 border-2 h-12 rounded-md pl-1"
                   />
                 </div>
                 <div className="grid">
                   <label htmlFor="exercise">Exercise</label>
                   <Field
                     name="exercise"
+                    id="exercise"
                     as="textarea"
-                    className="border-gray-500 border-2 h-12 rounded-md"
+                    className="border-gray-500 border-2 h-12 rounded-md pl-1"
                   />
                 </div>
                 <div className="grid">
-                  <label htmlFor="folow-up">Follow-up visit</label>
+                  <label htmlFor="followUp">Follow-up visit</label>
                   <Field
                     name="followUp"
+                    id="followUp"
                     type="text"
-                    className="border-gray-500 border-2 h-12 rounded-md"
+                    className="border-gray-500 border-2 h-12 rounded-md pl-1"
                   />
                 </div>
-                <button
-                  className="bg-green-500 text-white py-2 px-12 rounded-md"
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Save & Sign
-                </button>
+                <div className="space-x-4">
+                  <button
+                    className="bg-green-500 text-white py-2 px-12 rounded-md"
+                    type="submit"
+                    disabled={isSubmitting}
+                    name="signed"
+                    onClick={() => setCheckSigned(true)}
+                  >
+                    Save & Sign
+                  </button>
+                  <button
+                    className="outline outline-green-500 py-2 px-8 rounded-md"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Save unsigned
+                  </button>
+                </div>
               </Form>
             </div>
           )}
         </Formik>
-        <PreviousHistory />
       </div>
     </>
   );
